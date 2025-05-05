e0a484051308e078ffce8bd7501d4484fa5d414a
Sanjana M <142888033+SanjanaM719@users.noreply.github.com>
2025-05-05 14:19:30 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CompanyProfile
-
-class CompanyProfileForm(forms.ModelForm):
-    class Meta:
-        model = CompanyProfile
-        fields = ['company_name', 'address', 'subscription_plan']
